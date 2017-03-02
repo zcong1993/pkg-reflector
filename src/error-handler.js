7ed1cfbfae0c5ba5cb26b4e0ebc2d9bf322199c1
zcong1993 <1005997919@qq.com>
2017-03-02 17:40:26 +0800

    tweak updatecheck and success log

--- src/error-handler.js
+++ src/error-handler.js
@@ -3,6 +3,7 @@ import chalk from 'chalk'
 export default function errorHandler (err) {
   if (err.name === 'PkgError') {
     console.log(chalk.red(`\n ! ${err.message} \n`))
+    process.exit(1)
   } else {
     console.log(chalk.red(err.stack))
   }
