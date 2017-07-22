4ac0b75d180500d21c8a596dfd4412673d2a0b1f
zcong1993 <1005997919@qq.com>
2017-07-22 23:53:35 +0800

    use babylon parser js file

--- src/get-deps.js
+++ src/get-deps.js
@@ -1,7 +1,6 @@
-import readline from 'readline'
-import fs from 'fs'
 import globby from 'globby'
-import {cwd, isFile} from './utils'
+import konan from 'konan'
+import {cwd, isFile, readFile} from './utils'
 import PkgError from './pkg-error'
 
 export default function getDeps (input, flags) {
@@ -9,7 +8,7 @@ export default function getDeps (input, flags) {
   const allDeps = []
   files.forEach((file) => {
     if (isFile(cwd(file))) {
-      allDeps.push(getFileDeps(cwd(file), flags))
+      allDeps.push(getFileDeps1(cwd(file), flags))
     }
   })
   return Promise.all(allDeps)
@@ -22,34 +21,46 @@ export default function getDeps (input, flags) {
     })
 }
 
-function getFileDeps (file, flags) {
-  const isES = flags.es
-  const isAll = flags.all
-  const deps = []
-  // require\(\W+([\w-.]+)[\/\w]*[\W]+\)
-  const reg = /require\(['"]+([a-zA-z][\w-.]+)[\/\w]*['"]+\)/i // eslint-disable-line no-useless-escape
-  const regES = /^import[^."]+['"]+([a-zA-z][\w-.]+)[\/\w]*['"]+/i // eslint-disable-line no-useless-escape
+// function getFileDeps (file, flags) {
+//   const isES = flags.es
+//   const isAll = flags.all
+//   const deps = []
+//   // require\(\W+([\w-.]+)[\/\w]*[\W]+\)
+//   const reg = /require\(['"]+([a-zA-z][\w-.]+)[\/\w]*['"]+\)/i // eslint-disable-line no-useless-escape
+//   const regES = /^import[^."]+['"]+([a-zA-z][\w-.]+)[\/\w]*['"]+/i // eslint-disable-line no-useless-escape
 
-  const rl = readline.createInterface({
-    input: fs.createReadStream(file)
-  })
+//   const rl = readline.createInterface({
+//     input: fs.createReadStream(file)
+//   })
 
-  return new Promise((resolve) => {
-    rl.on('line', (line) => {
-      if (!isES || isAll) {
-        const matches = line.match(reg)
-        if (matches) {
-          deps.push(matches[1])
-        }
-      }
-      if (isES || isAll) {
-        const matchesES = line.match(regES)
-        if (matchesES) {
-          deps.push(matchesES[1])
-        }
-      }
-    }).on('close', () => {
-      return resolve(deps)
+//   return new Promise((resolve) => {
+//     rl.on('line', (line) => {
+//       if (!isES || isAll) {
+//         const matches = line.match(reg)
+//         if (matches) {
+//           deps.push(matches[1])
+//         }
+//       }
+//       if (isES || isAll) {
+//         const matchesES = line.match(regES)
+//         if (matchesES) {
+//           deps.push(matchesES[1])
+//         }
+//       }
+//     }).on('close', () => {
+//       return resolve(deps)
+//     })
+//   })
+// }
+
+function getFileDeps1 (file) {
+  const code = readFile(file)
+  const {strings} = konan(code)
+  return strings
+    .filter((module) => {
+      return !/^\./.test(module)
     })
-  })
+    .map((module) => {
+      return module.split('/')[0]
+    })
 }
