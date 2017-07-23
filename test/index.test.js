2455cc606082d9fb2844f7da5c5d5b53ac46e21a
zcong1993 <1005997919@qq.com>
2017-07-24 00:30:44 +0800

    add test

--- test/index.test.js
+++ test/index.test.js
@@ -2,21 +2,25 @@ const path = require('path')
 const spawn = require('cross-spawn')
 
 const testRoot = __dirname
-const testDir = path.resolve(__dirname, 'test')
-const testJson = path.resolve(testDir, 'package.json')
 
 const prf = path.resolve(__dirname, '../', 'bin', 'pkg-reflector')
 
-beforeEach(() => {
-  spawn.sync('cp', ['-a', './fixture', './test'], { cwd: testRoot })
-})
-
-afterEach(() => {
-  spawn.sync('rm', ['-rf', './test'], { cwd: testRoot })
-})
-
 test('default usage', () => {
+  const testDir = path.resolve(__dirname, 'test1')
+  const testJson = path.resolve(testDir, 'package.json')
+  spawn.sync('cp', ['-a', './fixture', './test1'], { cwd: testRoot })
   spawn.sync(prf, ['./*'], { cwd: testDir })
-  const pkg = require.resolve(testJson)
-  expect(pkg.dependencies).toMatchSnapshot()
+  const pkg = require(testJson)
+  expect(pkg).toMatchSnapshot()
+  spawn.sync('rm', ['-rf', './test1'], { cwd: testRoot })
+})
+
+test('save dev', () => {
+  const testDir = path.resolve(__dirname, 'test2')
+  const testJson = path.resolve(testDir, 'package.json')
+  spawn.sync('cp', ['-a', './fixture', './test2'], { cwd: testRoot })
+  spawn.sync(prf, ['./*', '-d'], { cwd: testDir })
+  const pkg = require(testJson)
+  expect(pkg).toMatchSnapshot()
+  spawn.sync('rm', ['-rf', './test2'], { cwd: testRoot })
 })
